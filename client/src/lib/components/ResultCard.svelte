2e4cfe37f375bd42b0eb9b54390cf8a012ee8bf0
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-06-04 04:16:45 +0800

    style: adjust formatting

--- client/src/lib/components/ResultCard.svelte
+++ client/src/lib/components/ResultCard.svelte
@@ -3,18 +3,14 @@
 </script>
 
 <article>
-	<h1>
-		Results
-	</h1>
-	
-	<slot name="diagnosis">
-	</slot>
-	
-	<br>
-	
+    <h1>Results</h1>
+
+    <slot name="diagnosis" />
+
+    <br />
+
     <span>Accuracy:</span>
-	<slot name="accuracy">
-	</slot>
+    <slot name="accuracy" />
 
     <div class="buttongroup">
         <Button type="secondary">Re-run</Button>
@@ -23,22 +19,22 @@
 </article>
 
 <style>
-	article {
+    article {
         margin: 1.75em;
-		padding: 1em;
+        padding: 1em;
         min-width: 25%;
         max-width: fit-content;
-		border: 2px solid var(--palai-black);
-		border-radius: 8px;
-		background-color: var(--palai-white);
-		color: var(--palai-black);
-		box-shadow: 2px 2px;
-	}
+        border: 2px solid var(--palai-black);
+        border-radius: 8px;
+        background-color: var(--palai-white);
+        color: var(--palai-black);
+        box-shadow: 2px 2px;
+    }
 
-	h1 {
-		padding-bottom: 0.25em;
-		border-bottom: 3px solid var(--palai-yellow);
-	}
+    h1 {
+        padding-bottom: 0.25em;
+        border-bottom: 3px solid var(--palai-yellow);
+    }
 
     .buttongroup {
         display: flex;
