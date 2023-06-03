896cc08e6c9cae32cc63163720e25ab59b9e8924
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-06-04 03:50:04 +0800

    refactor: adjust ResultCard styles

--- client/src/lib/components/ResultCard.svelte
+++ client/src/lib/components/ResultCard.svelte
@@ -16,14 +16,18 @@
 	<slot name="accuracy">
 	</slot>
 
-    <br>
-    <Button type="secondary">Re-run</Button>
-    <Button>Try another</Button>
+    <div class="buttongroup">
+        <Button type="secondary">Re-run</Button>
+        <Button>Try another</Button>
+    </div>
 </article>
 
 <style>
 	article {
+        margin: 1.75em;
 		padding: 1em;
+        min-width: 25%;
+        max-width: fit-content;
 		border: 2px solid var(--palai-black);
 		border-radius: 8px;
 		background-color: var(--palai-white);
@@ -35,4 +39,11 @@
 		padding-bottom: 0.25em;
 		border-bottom: 3px solid var(--palai-yellow);
 	}
+
+    .buttongroup {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+        margin-top: 1.25em;
+    }
 </style>
