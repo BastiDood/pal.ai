b4c1d7ae0fa13b0454ba26e4b18ce64a1cc1ce87
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-06-05 10:07:52 +0800

    feat(client): implement data models for classification

---
+++ client/src/models/Classification.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+
+export enum Disease {
+    BacterialLeafBlight = 'bacterial_leaf_blight',
+    BacterialLeafStreak = 'bacterial_leaf_streak',
+    Bakanae = 'bakanae',
+    BrownSpot = 'brown_spot',
+    GrassyStuntVirus = 'grassy_stunt_virus',
+    HealthyRicePlant = 'healthy_rice_plant',
+    NarrowBrownSpot = 'narrow_brown_spot',
+    RaggedStuntVirus = 'ragged_stunt_virus',
+    RiceBlast = 'rice_blast',
+    RiceFalseSmut = 'rice_false_smut',
+    SheathBlight = 'sheath_blight',
+    SheathRot = 'sheath_rot',
+    StemRot = 'stem_rot',
+    TungroVirus = 'tungro_virus',
+}
+
+export const DiseaseSchema = z.nativeEnum(Disease);
+
+export const ClassificationSchema = z
+    .object({ label: DiseaseSchema, score: z.number().min(0).max(1) })
+    .array()
+    .transform(classification => {
+        const pairs = classification.map(({ label, score }) => [label, score] as const);
+        return Object.fromEntries(pairs);
+    });
+
+export type Classification = z.infer<typeof ClassificationSchema>;
